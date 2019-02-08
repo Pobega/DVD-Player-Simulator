45919fdd2e49501e1bfa1bd538cc8ba2d8b40189
Michael Pobega <pobega@gmail.com>
2019-02-08 02:07:02 -0500

    Add changeColor function to sprite
    Added new member value 'imagemap' to allow this function to work.
    
    Renamed sprite.sprite => sprite.image for clarity.

--- src/sprite.lua
+++ src/sprite.lua
@@ -1,14 +1,21 @@
 local sprite = {}
 
 local function draw(self, x, y)
-  love.graphics.draw(self.sprite, x, y)
+  love.graphics.draw(self.image, x, y)
 end
 
-function sprite.create(image)
+local function changeColor(self, r, g, b, a)
+  local imagemap = self.imagedata:mapPixel(r, g, b, a)
+  self.image:replacePixels(imagemap)
+end
+
+function sprite.create(file)
   local inst = {}
 
   -- Member values
-  inst.sprite = love.graphics.newImage(image)
+  inst.filename = file
+  inst.imagedata = love.image.newImageData(file)
+  inst.image = love.graphics.newImage(inst.imagedata)
 
   -- Member methods
   inst.draw = draw
